443acd9953e127a02e8407fce94edd347ef1aa54
KI-Anik <iamkhairul101@gmail.com>
2025-02-24 16:00:22 +0600

    add axios-interceptor in useAxiosSecure hook to handle authentication and refactor MyApplications to use it for API calls

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import { useEffect } from 'react';
+import useAuth from './useAuth';
+import { useNavigate } from 'react-router-dom';
 
 const instance = axios.create({
     baseURL: 'http://localhost:5000',
@@ -6,6 +9,25 @@ const instance = axios.create({
 })
 
 const useAxiosSecure = () => {
+    const {signOutUser } = useAuth()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        instance.interceptors.response.use(response => {
+            return response
+        }, error => {
+            if (error.status === 401 || 403) {
+                return signOutUser()
+                .then(()=>{
+                    console.log('logout by intercepter')
+                    // redirect to login page
+                    navigate('/signin')
+                })
+                .catch(err =>console.log(err.message))
+            }
+            return Promise.reject(error)
+        })
+    }, [])
     return instance
 
 };
